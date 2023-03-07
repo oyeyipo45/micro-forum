4832352002855e5fd2f9fa85f19c742ceabb3853
damilola oyeyipo <oyeyipo45@gmail.com>
2023-03-07 21:18:47 +0100

    Fetch and display posts

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import matter from 'gray-matter';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 
@@ -18,8 +19,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 const readPosts = () => {
   const findFolder = path.join(process.cwd(), 'posts');
   const posts = fs.readdirSync(findFolder);
+  return posts.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), 'posts/' + filename);
+    const content = fs.readFileSync(filePathToRead, { encoding: 'utf-8' });
 
-  return posts;
+    return matter(content).data;
+  });
 };
 
 export default handler;
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,23 +1,27 @@
 import BlogCard from '@/components/BlogCard';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 interface Props {}
 
 const Blogs: NextPage<Props> = () => {
+  const [Posts, setPosts] = useState<{ title: string; meta: string; description: string }[]>([]);
+
+  const fetchPosts = async () => {
+    const { posts } = await fetch('/api/posts').then((data) => data.json());
+
+    setPosts(posts);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className=' max-w-3xl mx-auto p-5 space-y-5'>
-      <BlogCard
-        title='This is the blog'
-        description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum ex iusto! Deserunt in sequi corrupti tempora, aliquam quam? Natus, incidunt dolore esse dicta quasi, sed quo est molestias numquam, enim sunt ex fugit.'
-      />
-      <BlogCard
-        title='This is the blog'
-        description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum ex iusto! Deserunt in sequi corrupti tempora, aliquam quam? Natus, incidunt dolore esse dicta quasi, sed quo est molestias numquam, enim sunt ex fugit.'
-      />
-      <BlogCard
-        title='This is the blog'
-        description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum ex iusto! Deserunt in sequi corrupti tempora, aliquam quam? Natus, incidunt dolore esse dicta quasi, sed quo est molestias numquam, enim sunt ex fugit.'
-      />
+      {Posts &&
+        Posts.map((post) => {
+          return <BlogCard key={post.title} title={post.title} description={post.meta} />;
+        })}
     </div>
   );
 };
